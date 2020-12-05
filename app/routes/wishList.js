446f9e93b2f0753f412c4c6e3fce83cd1866d9a7
Maaz Ahmed <maazahmed@Maazs-iMac.local>
2020-12-04 22:20:51 -0800

    updated

--- app/routes/wishList.js
+++ app/routes/wishList.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const router = express.Router()
 const User = require("../models/userModel");
+const ProductModal = require("../models/ProductModal")
+
 
 
 
@@ -9,11 +11,11 @@ router.post("/add", (req, res) => {
         .then((data) => {
             if (data[0]) {
                 let wishlist = data[0].wishlist;
-                const productId =  req.body.productId;
+                const productId = req.body.productId;
                 if (wishlist) {
-                    const X = wishlist.find(x => x.productId === productId)
+                    const X = wishlist.find(x => x === productId)
                     if (X == undefined) {
-                        wishlist.push({ productId: productId })
+                        wishlist.push(productId)
                         User.updateOne({ _id: req.body._id },
                             { $set: { "wishlist": wishlist } }, (error, success) => {
                                 res.send({
@@ -22,7 +24,7 @@ router.post("/add", (req, res) => {
                                 })
                             })
                     } else {
-                        const X_filter = wishlist.filter(x => x.productId !== productId)
+                        const X_filter = wishlist.filter(x => x !== productId)
                         User.updateOne({ _id: req.body._id },
                             { $set: { "wishlist": X_filter } }, (error, success) => {
                                 res.send({
@@ -33,7 +35,7 @@ router.post("/add", (req, res) => {
                     }
                 } else {
                     User.updateOne({ _id: req.body._id },
-                        { $set: { "wishlist": [{ productId: productId }] } }, (error, success) => {
+                        { $set: { "wishlist": [productId] } }, (error, success) => {
                             res.send({
                                 code: 200,
                                 success
@@ -51,6 +53,21 @@ router.post("/add", (req, res) => {
             })
         })
 
+})
+
+router.get("/get", (req, res) => {
+    ProductModal.find({ _id: { $in:req.body.productIds } }).exec()
+        .then((data) => {
+            res.send({
+                data,
+                code:200
+            })
+        }).catch((err)=>{
+            res.send({
+                error:err,
+                code:300
+            })
+        })
 })
 
 
